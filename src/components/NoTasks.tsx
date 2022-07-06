e04d25a6c383710511a40336519750e0c1c68e47
Claudio Alberto <claudio.goncalves@tesla.com.br>
2022-07-06 16:46:28 -0300

    Improvements in Mobile Layout

--- src/components/NoTasks.tsx
+++ src/components/NoTasks.tsx
@@ -2,7 +2,7 @@ import { Clipboard } from "./ClipboardIcon";
 
 export function NoTasks() {
     return (
-        <div className="flex border-t rounded-lg border-gray-400 mt-6 flex-col items-center text-center justify-center pt-16">
+        <div className="flex border-t rounded-lg border-gray-400 mt-6 flex-col items-center text-center justify-center py-16">
             <Clipboard />
             <strong
                 className="text-gray-300 mt-4"
